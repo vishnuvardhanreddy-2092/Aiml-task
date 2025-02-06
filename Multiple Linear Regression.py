8e1536933425b814d9c5b301f4cdaa6dc805ae53
vishnuvardhanreddy-2092 <vishnuvardhanreddy842@gmail.com>
2025-02-06 10:26:53 +0530

    Add files via upload

--- Multiple Linear Regression.py
+++ Multiple Linear Regression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 import pandas as pd
@@ -12,7 +12,7 @@ from statsmodels.graphics.regressionplots import influence_plot
 import numpy as np
 
 
-# In[5]:
+# In[2]:
 
 
 # Read the data from csv file
@@ -20,12 +20,192 @@ cars = pd.read_csv("Cars.csv")
 cars.head()
 
 
-# In[7]:
+# In[3]:
 
 
 # Rearange the columns
 cars = pd.DataFrame(cars, columns=["HP","VOL","SP", "WT","MPG"])
 cars.head()
+
+
+# ### Asumptions in Multilinear Regression
+# 1.Linearity:The relationship between the predictors(x) and the response (y) is linear.
+# 2.Independence:Observation are independent of each other.
+# 3.Homoscedasticity:The residuals (Y - Y_hat) exhibit constant variance at all levels of the predictors
+# 4.Normal Distribustion of Errors:The residuals of the model are normally distributed.
+# 5.No multicollinearity:The independent variables should not be too highly correlated with each other.
+# Voilations of these assumptions
+
+# ## EDA
+
+# In[4]:
+
+
+cars.info()
+
+
+# In[5]:
+
+
+#Check for missing values
+cars.isna().sum()
+
+
+# ## Observations about info(), missing values
+# - There are no missing values
+# - There are 81 obsevations (81 different cars data)
+# - The data types of the columns are also relevant and valid
+
+# In[6]:
+
+
+fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
+
+# Creating a boxplot
+sns.boxplot(data=cars, x='HP', ax=ax_box, orient='h')
+ax_box.set(xlabel='') # Remove x label for the boxplot
+
+# Creating a histogram in the same x-axis
+sns.histplot(data=cars, x='HP', ax=ax_hist, bins=30, kde=True, stat="density")
+ax_hist.set(ylabel='Density')
+
+# Adjust Layout
+plt.tight_layout()
+plt.show()
+
+
+# In[7]:
+
+
+fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
+
+# Creating a boxplot
+sns.boxplot(data=cars, x='SP', ax=ax_box, orient='h')
+ax_box.set(xlabel='') # Remove x label for the boxplot
+
+# Creating a histogram in the same x-axis
+sns.histplot(data=cars, x='SP', ax=ax_hist, bins=30, kde=True, stat="density")
+ax_hist.set(ylabel='Density')
+
+# Adjust Layout
+plt.tight_layout()
+plt.show()
+
+
+# In[8]:
+
+
+fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
+
+# Creating a boxplot
+sns.boxplot(data=cars, x='WT', ax=ax_box, orient='h')
+ax_box.set(xlabel='') # Remove x label for the boxplot
+
+# Creating a histogram in the same x-axis
+sns.histplot(data=cars, x='WT', ax=ax_hist, bins=30, kde=True, stat="density")
+ax_hist.set(ylabel='Density')
+
+# Adjust Layout
+plt.tight_layout()
+plt.show()
+
+
+# In[9]:
+
+
+fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
+
+# Creating a boxplot
+sns.boxplot(data=cars, x='MPG', ax=ax_box, orient='h')
+ax_box.set(xlabel='') # Remove x label for the boxplot
+
+# Creating a histogram in the same x-axis
+sns.histplot(data=cars, x='MPG', ax=ax_hist, bins=30, kde=True, stat="density")
+ax_hist.set(ylabel='Density')
+
+# Adjust Layout
+plt.tight_layout()
+plt.show()
+
+
+# In[10]:
+
+
+fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
+
+# Creating a boxplot
+sns.boxplot(data=cars, x='VOL', ax=ax_box, orient='h')
+ax_box.set(xlabel='') # Remove x label for the boxplot
+
+# Creating a histogram in the same x-axis
+sns.histplot(data=cars, x='VOL', ax=ax_hist, bins=30, kde=True, stat="density")
+ax_hist.set(ylabel='Density')
+
+# Adjust Layout
+plt.tight_layout()
+plt.show()
+
+
+# ### Observations from boxplot and histogrms
+# - There are some extreme values (outliers) observed in towards the right tail of SP and HP distributions.
+# - In VOL and WT columns,a few outliers are observed in both tails of their distributions.
+# - The extreme values of cars data may have come from the specially designed nature of cars.
+# - As this is multi-dimensinal data, the outliers with respect to spatial dimensions may have to be considered while bulding the regression model
+
+# In[11]:
+
+
+# Checking for duplicated rows
+cars[cars.duplicated()]
+
+
+# #### Pair plots and Correlation Coefficients
+
+# In[12]:
+
+
+sns.set_style(style='darkgrid')
+sns.pairplot(cars)
+
+
+# ### Observation from correlation plots and Coefficients
+# - Between x and y, all the variables are showing moderate to high correlation strengths, highest being between HP and MPG
+# - Therefore this columns (x1,x2,x3 and x4), some very high correlation strengths are observed between SP vs HP,VOL vs WT
+# - The high correlation among x columns is not desirable as it might lead to multicollinearity problem
+
+# In[13]:
+
+
+#Build model
+#import statsmodels.formula.api as smf
+model1 = smf.ols('MPG~WT+VOL+SP+HP',data=cars).fit()
+
+
+# In[15]:
+
+
+model1.summary()
+
+
+# ### Observations from model summary
+# - The R-squared and adjusted R-suared values are good and about 75% of variability in Y is explained by X columns
+# - The probability value with respect to F-statistic is close to zero, indicating that all or some of X columns are significant
+# - The p-values for VOL and WT are higher than 5% indicating some interaction issue among themselves, which need to be further explored
+
+# In[20]:
+
+
+df1 = pd.DataFrame()
+df1["actual_y1"] = cars["MPG"]
+df1.head()
+
+
+# In[22]:
+
+
+pred_y1 = model1.predict(cars.iloc[:,0:4])
+df1["pred_y1"] = pred_y1
+df1.head()
 
 
 # In[ ]:
