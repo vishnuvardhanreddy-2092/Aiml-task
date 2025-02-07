665dc51ad6fb1441bf648f2ef37a5062d3a59a13
vishnuvardhanreddy-2092 <vishnuvardhanreddy842@gmail.com>
2025-02-07 12:58:54 +0530

    Add files via upload

--- Multiple Linear Regression (1).py
+++ Multiple Linear Regression (1).py
@@ -38,13 +38,13 @@ cars.head()
 
 # ## EDA
 
-# In[5]:
+# In[4]:
 
 
 cars.info()
 
 
-# In[6]:
+# In[5]:
 
 
 #Check for missing values
@@ -56,7 +56,7 @@ cars.isna().sum()
 # - There are 81 obsevations (81 different cars data)
 # - The data types of the columns are also relevant and valid
 
-# In[10]:
+# In[6]:
 
 
 fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
@@ -74,7 +74,7 @@ plt.tight_layout()
 plt.show()
 
 
-# In[12]:
+# In[7]:
 
 
 fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
@@ -92,7 +92,7 @@ plt.tight_layout()
 plt.show()
 
 
-# In[13]:
+# In[8]:
 
 
 fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
@@ -110,7 +110,7 @@ plt.tight_layout()
 plt.show()
 
 
-# In[15]:
+# In[9]:
 
 
 fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
@@ -128,7 +128,7 @@ plt.tight_layout()
 plt.show()
 
 
-# In[17]:
+# In[10]:
 
 
 fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
@@ -152,7 +152,7 @@ plt.show()
 # - The extreme values of cars data may have come from the specially designed nature of cars.
 # - As this is multi-dimensinal data, the outliers with respect to spatial dimensions may have to be considered while bulding the regression model
 
-# In[18]:
+# In[11]:
 
 
 # Checking for duplicated rows
@@ -161,11 +161,148 @@ cars[cars.duplicated()]
 
 # #### Pair plots and Correlation Coefficients
 
-# In[19]:
+# In[12]:
 
 
 sns.set_style(style='darkgrid')
 sns.pairplot(cars)
 
 
-# 
+# ### Observation from correlation plots and Coefficients
+# - Between x and y, all the variables are showing moderate to high correlation strengths, highest being between HP and MPG
+# - Therefore this columns (x1,x2,x3 and x4), some very high correlation strengths are observed between SP vs HP,VOL vs WT
+# - The high correlation among x columns is not desirable as it might lead to multicollinearity problem
+
+# In[13]:
+
+
+#Build model
+#import statsmodels.formula.api as smf
+model1 = smf.ols('MPG~WT+VOL+SP+HP',data=cars).fit()
+
+
+# In[14]:
+
+
+model1.summary()
+
+
+# ### Observations from model summary
+# - The R-squared and adjusted R-suared values are good and about 75% of variability in Y is explained by X columns
+# - The probability value with respect to F-statistic is close to zero, indicating that all or some of X columns are significant
+# - The p-values for VOL and WT are higher than 5% indicating some interaction issue among themselves, which need to be further explored
+
+# In[15]:
+
+
+df1 = pd.DataFrame()
+df1["actual_y1"] = cars["MPG"]
+df1.head()
+
+
+# In[16]:
+
+
+pred_y1 = model1.predict(cars.iloc[:,0:4])
+df1["pred_y1"] = pred_y1
+df1.head()
+
+
+# In[20]:
+
+
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(df1["actual_y1"], df1["pred_y1"])
+print("MSE :", mse)
+print("RMSE :",np.sqrt(mse))
+
+
+
+# ### Check for multicollinearity among X-columns using VIF metho
+
+# In[21]:
+
+
+# Compute VIF values
+rsq_hp = smf.ols('HP~WT+VOL+SP',data=cars).fit().rsquared
+vif_hp = 1/(1-rsq_hp)
+
+rsq_wt = smf.ols('WT~HP+VOL+SP',data=cars).fit().rsquared  
+vif_wt = 1/(1-rsq_wt) 
+
+rsq_vol = smf.ols('VOL~WT+SP+HP',data=cars).fit().rsquared  
+vif_vol = 1/(1-rsq_vol) 
+
+rsq_sp = smf.ols('SP~WT+VOL+HP',data=cars).fit().rsquared  
+vif_sp = 1/(1-rsq_sp) 
+
+# Storing vif values in a data frame
+d1 = {'Variables':['Hp','WT','VOL','SP'],'VIF':[vif_hp,vif_wt,vif_vol,vif_sp]}
+Vif_frame = pd.DataFrame(d1)  
+Vif_frame
+
+
+# ### Observations for VIF values:
+# - The ideal range of VIF values shall be between 0 to 10. However slightly higher values can be tolerated
+# - As seen from the very high VIF values for VOL and WT, it is clear that they are prone to multicollinearity problem.
+# - Hence it is decided to drop one of the columns (either VOL or WT) to overcome the multicollinearity.
+# - It is decided to drop WT and retian VOL column in further models
+
+# In[23]:
+
+
+cars1 = cars.drop("WT", axis=1)
+cars1.head()
+
+
+# In[27]:
+
+
+import statsmodels.formula.api as smf
+model2 = smf.ols('MPG~VOL+SP+HP',data=cars1).fit()
+
+
+# In[28]:
+
+
+model2.summary()
+
+
+# ### Performance metrics form model2
+
+# In[29]:
+
+
+df2 = pd.DataFrame()
+df2["actual_y2"] = cars["MPG"]
+df2.head()
+
+
+# In[30]:
+
+
+pred_y2 = model2.predict(cars1.iloc[:,0:4])
+df2["pred_y2"] = pred_y2
+df2.head()
+
+
+# In[32]:
+
+
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(df2["actual_y2"], df2["pred_y2"])
+print("MSE :", mse)
+print("RMSE :",np.sqrt(mse))
+
+
+# ### Observations from model2 summary()
+# - The adjusted R-squared value improved slightly to 0.76
+# - All the p-values for model parameters are less than 5% hence they are significant
+# - Therefore the HP,VOL,SP columns are finalized as the significant predictor for the MPG response variable
+# - There is no improvement in MSE value
+
+# In[ ]:
+
+
+
+
